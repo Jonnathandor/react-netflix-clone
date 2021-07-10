f42d9721d89b933b37300011a297c719db8b8167
Jonnathan Edgardo Garcia Galeas <jonnathandor@gmail.com>
2021-07-10 18:00:18 -0500

    added a method to fetch images on row load

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,10 +1,32 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from './axios';
 
-export default function Row({ title }) {
+const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/original/';
+
+export default function Row({ title, fetchURL }) {
     const [movies, setMovies] = useState([]);
+
+    // runs based on a specific condition in this case
+    // make a request when the row loads on the DOM
+    useEffect(() => {
+        async function fetchData(){
+            const request = await axios.get(fetchURL);
+            console.log(request.data.results);
+            setMovies(request.data.results);
+            return request;
+        }
+
+        fetchData();
+    }, [fetchURL]);
+
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
+            <div className="row__posters">
+                {movies.map(movie => {
+                    return <img className="row__poster" src={`${BASE_IMAGE_URL}${movie.poster_path}`} alt={movie.name}></img>
+                })}
+            </div>
         </div>
     )
 }
